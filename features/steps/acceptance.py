e51b09842855d62398b694ec9805b8909ce6317a
Andrey O <10812297+andreyors@users.noreply.github.com>
2022-04-10 16:07:06 +0200

    Add pre-commit

--- features/steps/acceptance.py
+++ features/steps/acceptance.py
@@ -1,16 +1,20 @@
-import requests
-from behave import *
+from __future__ import annotations
+
+from behave import given, then, when
+from requests import session
+
 
 @given("we have web server started")
-def step_impl(context):
+def step_impl1(context):
     pass
 
+
 @when('we do a HTTP request to "{url}"')
-def step_impl(context, url):
-    context.response = requests.session().get(url)
+def step_impl2(context, url):
+    context.response = session().get(url)
+
 
 @then('we receive "{message}"')
-def step_impl(context, message):
+def step_impl3(context, message):
     assert context.response.status_code == 200
     assert context.response.text == message
-
